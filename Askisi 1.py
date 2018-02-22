da2a3914e954951d3c286d98a9ef17d7b9947fc5
Giorgos Prokopi <giorgos.prokopi@gmail.com>
2018-02-22 19:32:17 +0200

    #2 test
    Filling a list with 80 numbers

--- Askisi 1.py
+++ Askisi 1.py
@@ -1,3 +1,10 @@
 import random
-a = []
-a = list(range(1,81)
+counter=0
+rounds=0
+
+def filling():
+        for i in range(1,81):
+            numbers80.insert(i,i+1)
+        return numbers80
+
+  
